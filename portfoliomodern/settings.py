970a7cc880f1db0c649e7a4a784309a9c8b30372
Affan Talib <affansukhera99@gmail.com>
2024-07-03 23:07:10 +0500

    new changings

--- portfoliomodern/settings.py
+++ portfoliomodern/settings.py
@@ -1,6 +1,8 @@
-
+import environ
 
 from pathlib import Path
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -10,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-og()pdy^w(5x^a$trg7-em#1v%v)3e7a&u#6xj(ffona6z9k*6'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -115,12 +117,12 @@ STATICFILES_DIRS = [
 ]
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER ='affansukhera99@gmail.com'
-EMAIL_HOST_PASSWORD = '**** **** **** ****'
+EMAIL_HOST_USER = env('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
-RECIPIENT_EMAIL = 'affansukhera00@gmail.com'
+RECIPIENT_EMAIL = env('RECIPIENT_EMAIL')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
